8f4093e76b54e830b872022004413566b1b82828
Adrien Qi <aqi@Adriens-MacBook-Air.local>
2022-12-30 17:41:54 -0500

    search_for_links

--- main.cpp
+++ main.cpp
@@ -4,61 +4,42 @@
 
 #include "gumbo.h"
 
-std::ofstream writeCsv("links.csv");
-
-std::string extract_html_page()
+std::string extract_html_page(std::string link)
 {
-    cpr::Url url = cpr::Url{"https://en.wikipedia.org/wiki/Poppy_seed_defence"};
+    cpr::Url url = cpr::Url{link};
     cpr::Response response = cpr::Get(url);
     return response.text;
 }
 
-void search_for_title(GumboNode* node)
-{
-    if (node->type != GUMBO_NODE_ELEMENT)
+void search_for_links(GumboNode* node, std::ofstream& writeCsv) {
+    if (node->type != GUMBO_NODE_ELEMENT) {
         return;
+    }
 
-    if (node->v.element.tag == GUMBO_TAG_H1)
-    {
-        GumboNode* title_text = static_cast<GumboNode*>(node->v.element.children.data[0]);
-        std::cout << title_text->v.text.text << "\n";
-        return;
+    GumboAttribute* href;
+    if (node->v.element.tag == GUMBO_TAG_A &&
+        (href = gumbo_get_attribute(&node->v.element.attributes, "href"))) {
+        writeCsv << href->value << std::endl;
     }
 
     GumboVector* children = &node->v.element.children;
-    for (unsigned int i = 0; i < children->length; i++)
-        search_for_title(static_cast<GumboNode*>(children->data[i]));
-}
-
-void search_for_links(GumboNode* node)
-{
-    if (node->type != GUMBO_NODE_ELEMENT)
-        return;
-
-    if (node->v.element.tag == GUMBO_TAG_A)
-    {
-        GumboAttribute* href = gumbo_get_attribute(&node->v.element.attributes, "href");
-        if (href)
-            std::cout << href->value << "\n";
-    }
-
-    GumboVector* children = &node->v.element.children;
-    for (unsigned int i = 0; i < children->length; i++)
-    {
-        search_for_links(static_cast<GumboNode*>(children->data[i]));
+    for (unsigned int i = 0; i < children->length; ++i) {
+        search_for_links(static_cast<GumboNode*>(children->data[i]), writeCsv);
     }
 }
 
-int main() {
-    std::string page_content = extract_html_page();
+int main(int argc, char** argv) {
+
+    std::ofstream writeCsv("links.csv");
+    std::string link;
+
+    std::cout << "Enter link" << std::endl;
+    std::cin >> link;
+
+    std::string page_content = extract_html_page(link);
 
     GumboOutput* parsed_response = gumbo_parse(page_content.c_str());
-
-    search_for_title(parsed_response->root);
-
+    search_for_links(parsed_response->root, writeCsv);
     // free the allocated memory
-
     gumbo_destroy_output(&kGumboDefaultOptions, parsed_response);
-
-    std::cout << "testing" << std::endl;
 }
